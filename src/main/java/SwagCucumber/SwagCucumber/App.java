8f026b2429280141d5c0f253da117e400837a1fc
abgavali99 <12abhishekgavali@gmail.com>
2021-11-22 12:39:18 +0000

    Saucelab Saucedemo automate using Cucumber final project

---
+++ src/main/java/SwagCucumber/SwagCucumber/App.java
@@ -0,0 +1,13 @@
+package SwagCucumber.SwagCucumber;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
